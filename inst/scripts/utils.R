1501b951813b6e403a5f25eda307b0873e3404cf
Christian <cholland2408@gmail.com>
2020-03-17 16:41:11 +0100

    Minor changes

--- inst/scripts/utils.R
+++ inst/scripts/utils.R
@@ -1,10 +1,10 @@
 ## TF properties
 load_tf_census = function(){
-  message('Load TF census')
-  tf_census = read_delim('inst/extdata/annotations/human_tf_census.txt',
+  message("Load TF census")
+  tf_census = read_delim("inst/extdata/annotations/human_tf_census.txt",
                          delim = "\t", col_names = "tf") %>%
     distinct(tf) %>%
     pull(tf)
-  
+
   return(tf_census)
 }
