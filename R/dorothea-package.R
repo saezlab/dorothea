6be577cea70b41bc44f81f4f83ca358f632a5890
Christian <cholland2408@gmail.com>
2020-05-01 15:37:13 +0200

    updated package documentation

---
+++ R/dorothea-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+#' @description{
+#' \if{html}{\figure{tool_logo.png}{options: align='right' alt='logo' width='120'}}
+#' This package contains human and mouse TF regulons.
+#'
+#' }
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
